9b24b321121b9a04a5132843d638722cf9353435
wangguji <tianlang1997@126.com>
2019-05-04 14:12:18 +0800

    redis 集群与单点适配 MongoDB 连接适配

---
+++ connector/redisClient/client.go
@@ -0,0 +1,105 @@
+package redisClient
+
+import (
+	"../../config"
+	"fmt"
+	"github.com/go-redis/redis"
+	"time"
+)
+
+type RedisClient struct {
+	clusterClient *redis.ClusterClient
+	client *redis.Client
+}
+
+func (this *RedisClient) Do(args...interface{})  *redis.Cmd {
+	if this.clusterClient != nil {
+		return this.clusterClient.Do(args)
+	}
+	if this.client != nil {
+		return this.client.Do(args)
+	}
+	panic("redis no ready!")
+	return nil
+}
+
+func (this *RedisClient) Get(key string) *redis.StringCmd {
+	if this.clusterClient != nil {
+		return this.clusterClient.Get(key)
+	}
+	if this.client != nil {
+		return this.client.Get(key)
+	}
+	panic("redis no ready!")
+	return nil
+}
+
+func (this *RedisClient) Set(key string, value interface{}, expiration time.Duration) *redis.StatusCmd {
+	if this.clusterClient != nil {
+		return this.clusterClient.Set(key,value,expiration)
+	}
+	if this.client != nil {
+		return this.client.Set(key,value,expiration)
+	}
+	panic("redis no ready!")
+	return nil
+}
+
+func (this *RedisClient) SetNX(key string, value interface{}, expiration time.Duration) *redis.BoolCmd {
+	if this.clusterClient != nil {
+		return this.clusterClient.SetNX(key,value,expiration)
+	}
+	if this.client != nil {
+		return this.client.SetNX(key,value,expiration)
+	}
+	panic("redis no ready!")
+	return nil
+}
+
+
+
+var messageClient,commonClient *RedisClient
+func GetMessageClient() *RedisClient {
+	return messageClient
+}
+
+func GetCommonClient() *RedisClient  {
+	return commonClient
+}
+
+func init()  {
+	messageClient = &RedisClient{}
+	initClient(messageClient,config.RedisClient.GetMessage())
+
+	commonClient = &RedisClient{}
+	initClient(commonClient,config.RedisClient.GetMessage())
+
+}
+
+func initClient(client *RedisClient,addrs []string)  {
+	client.clusterClient = redis.NewClusterClient(&redis.ClusterOptions{
+		Addrs: addrs,
+		ReadTimeout:  50 * time.Millisecond,
+		WriteTimeout: 50 * time.Millisecond,
+		DialTimeout: 50 * time.Millisecond,
+		PoolSize: 100,
+	})
+
+	pong, err := client.clusterClient.Ping().Result()
+	fmt.Println(pong, err)
+	if err != nil{
+		panic(err)
+
+		client.client = redis.NewClient(&redis.Options{
+			Addr:     addrs[0],
+			Password: "", // no password set
+			DB:       0,  // use default DB
+		})
+
+		pong, err := client.client.Ping().Result()
+		fmt.Println(pong, err)
+		if err != nil {
+			panic(err)
+		}
+	}
+}
---
+++ database/user.go
@@ -0,0 +1,34 @@
+package database
+
+import (
+	"../connector/mongodbClient"
+	"gopkg.in/mgo.v2/bson"
+	"../datastruct"
+)
+
+type userData struct {
+	db string
+	collection string
+}
+
+func (this * userData) GetByName(name string)(error,datastruct.User)  {
+	var result datastruct.User
+	var err = mongodbClient.FindOne(this.db,this.collection,bson.M{"name": name}, bson.M{"_id":0}, &result)
+	return err,result
+}
+
+func (this * userData) Insert(user *datastruct.User) error  {
+	var err = mongodbClient.Insert(this.db,this.collection,user);
+	return err
+}
+var userDataInstance *userData
+
+func init()  {
+	userDataInstance=&userData{
+		db:"testdb",
+		collection:"user",
+	}
+}
+func GetUserData()(* userData)  {
+	return userDataInstance
+}
